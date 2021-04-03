fdfd79083656416ff1b1db6e462cedc082a80a19
Larissa <larissa.sena@grupomfx.com>
2021-04-03 15:39:38 -0300

    ignore node_modules

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,14 +1,23 @@
+const moment = require('moment');
 const conexao = require('../infra/conexao');
 
 class Atendimento {
-    adiciona(atendimento) {
+    adiciona(atendimento, res) {
+        const dataCriacao = moment().format('YYYY-MM-DD hh:mm:ss');
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD hh:mm:ss');
+
+        // validações
+        const dataValida = moment(data).isSameOrAfter(dataCriacao);
+        const clienteValido = atendimento.cliente.legth >= 3;
+
+        const atendimentoDatado = {...atendimento, dataCriacao, data};
         const sql = 'INSERT INTO Atendimentos SET ?';
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
+        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
             if (erro) {
-                console.log(erro);
+                res.status(400).json(erro);
             } else {
-                console.log(resultados);
+                res.status(201).json(resultados);
             }
         });
     }
