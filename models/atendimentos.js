0e3d9b7c9b1cd3a4b792c59cad0405b5926dc05c
Larissa <larissa.sena@grupomfx.com>
2021-04-03 16:36:00 -0300

    4th class: format dates with moment, fix http statuses, validate inputs

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -8,18 +8,36 @@ class Atendimento {
 
         // validações
         const dataValida = moment(data).isSameOrAfter(dataCriacao);
-        const clienteValido = atendimento.cliente.legth >= 3;
-
-        const atendimentoDatado = {...atendimento, dataCriacao, data};
-        const sql = 'INSERT INTO Atendimentos SET ?';
-
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if (erro) {
-                res.status(400).json(erro);
-            } else {
-                res.status(201).json(resultados);
+        const clienteValido = atendimento.cliente.length >= 3;
+        const validacao = [
+            {
+                nome: 'data',
+                valido: dataValida,
+                mensagem: 'Data deve ser igual ou posterior à data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteValido,
+                mansagem: 'Cliente deve ter pelo menos 3 caracteres'
             }
-        });
+        ];
+        const erros = validacao.filter(campo => !campo.valido);
+        const existemErros = erros.length;
+
+        if (existemErros) {
+            res.status(400).json(erros);
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data};
+            const sql = 'INSERT INTO Atendimentos SET ?';
+
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if (erro) {
+                    res.status(400).json(erro);
+                } else {
+                    res.status(201).json(resultados);
+                }
+            });
+        }
     }
 }
 
